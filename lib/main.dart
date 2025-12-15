ff5a9d8b7abe66860e9d079a741f6fbaebb4c549
Luong Dung <luongdinhdung003hy@gmail.com>
2025-12-15 13:56:01 +0700

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:lingua_socialnetwork/pages/home.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,7 +15,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: Home(),
+      //  home: Home(),
     );
   }
 }
